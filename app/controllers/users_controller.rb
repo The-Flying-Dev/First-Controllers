6857debf6b16a64fe525ab1fe855ec15a2b453af
Leandran <leandranm@outlook.com>
2021-10-26 01:32:54 +0200

    update cuser controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,31 @@
 class UsersController < ApplicationController
+    before_action :find_user, only:[:show, :edit, :update, :destroy]
+
     def index 
         @users = User.all
         render json: @users
     end
 
+
     def show 
         #render json: params
-        @user = User.find(params(:id))
+        #@user = User.find(params(:id))
     end
+
 
     def new 
         @user = User.new
     end
 
+
     def edit 
-        user = User.find(params(:id))
+        #user = User.find(params(:id))
     end
     
 
     def create 
         #user = User.new(params.require(user).permit(:name,email))
+
         user = User.new(user_params)
         if user.save!
             render json: user
@@ -28,8 +34,9 @@ class UsersController < ApplicationController
         end
     end
 
+
     def update 
-        user = User.find(params(:id))
+        #user = User.find(params(:id))
         if user.update!(user_params)
             render json: user
         else 
@@ -37,13 +44,22 @@ class UsersController < ApplicationController
         end
     end
 
+
     def destroy 
-        user = user.destroy
+        #user = User.find(params(:id))
+        user.destroy
     end
 
     
 
     private 
+    #this action locates the user instance in the database
+    def find_user
+        user = User.find(params(:id))
+    end
+
+
+    #this action only permits the following attributes to be entered in by the user, called whitelisting
     def user_params
         params.require(:user).permit(:name, :email)
     end
